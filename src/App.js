4559ec0067ce840f5507da20a901f0c3960c0b33
Gunjan Solanki <gunjan.solanki@bacancy.com>
2022-07-18 15:26:32 +0530

    Redirect after signin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/header/header.component';
@@ -41,7 +41,15 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route 
+            exact
+            path='/signin' 
+            render={() => 
+              this.props.currentUser ?
+                <Redirect to='/' /> :
+                <SignInAndSignUpPage />
+            }
+          />
         </Switch>
         
       </div>
@@ -49,9 +57,13 @@ class App extends React.Component {
     );
   }
 }
- 
+
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,12 @@
+import { UserActionTypes } from "./user.actionTypes";
+
 const intialState = {
   currentUser: null
 }
 
 const userReducer = (state=intialState, action) => {
   switch(action.type) {
-    case "SET_CURRENT_USER": 
+    case UserActionTypes.SET_CURRENT_USER: 
       return {
         ...state,
         currentUser: action.payload
