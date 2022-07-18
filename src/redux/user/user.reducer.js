ef5f4cc456d2c3e2aeed0af07ce305f1245e1d3a
Gunjan Solanki <gunjan.solanki@bacancy.com>
2022-07-18 15:06:19 +0530

    Redux, middleware, actions

---
+++ src/redux/user/user.reducer.js
@@ -0,0 +1,18 @@
+const intialState = {
+  currentUser: null
+}
+
+const userReducer = (state=intialState, action) => {
+  switch(action.type) {
+    case "SET_CURRENT_USER": 
+      return {
+        ...state,
+        currentUser: action.payload
+      }
+    default:
+      return state;
+  }
+    
+}
+
+export { userReducer };
