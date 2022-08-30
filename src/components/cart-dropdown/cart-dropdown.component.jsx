7d2b01af56103863e46e45edafcbe5f52e90c658
Gunjan Solanki <gunjan.solanki@bacancy.com>
2022-08-30 10:59:55 +0530

    Cart dropdown & items

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.styles.scss';
+import { toggleCartHidden } from '../../redux/cart/cart-actions';
+import CartItem from '../cart-item/cart-item.component';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
-    <div className='cart-items' />
-    <CustomButton>CHECKOUT</CustomButton>
+    <div className='cart-items'>
+      {cartItems.length ? (
+        cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      ) : (
+        <span className='empty-message'>Your cart is empty</span>
+      )}
+    </div>
+    <CustomButton
+      onClick={() => {
+        history.push('/checkout');
+        dispatch(toggleCartHidden());
+      }}
+    >
+      GO TO CHECKOUT
+    </CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({cart: { cartItems }}) => ({
+  cartItems
+})
+
+export default withRouter(connect(mapStateToProps)(CartDropdown));
